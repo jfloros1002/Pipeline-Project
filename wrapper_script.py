98f1ba328d060e599f76d7c91e7b3e16d9c3f32b
John Floros <jfloros1002@gmail.com>
2025-02-22 22:46:28 -0600

    Completed database lookup

--- wrapper_script.py
+++ wrapper_script.py
@@ -1,8 +1,15 @@
 #Import Necessary Dependencies
 import os
+import subprocess
 #Create a directory for output of all files
 os.system("mkdir PipelineProject_John_Floros")
 os.chdir("PipelineProject_John_Floros")
-HCMV_cds = os.system("datasets download virus  genome accession NC_006273.2 --include cds")
+os.system("echo > PipelineProject.log")
 
+HCMV_cds = os.system("datasets download virus  genome accession NC_006273.2 --include cds")
+os.system("unzip ncbi_dataset.zip")
+os.chdir("ncbi_dataset/data")
+genomic_CDS_num_command = subprocess.run("grep -c '>' cds.fna", shell=True, capture_output=True, text=True)
+genomic_CDS_num = genomic_CDS_num_command.stdout.strip()
+print(genomic_CDS_num)
 #reference_transcriptome = "kallisto index -1 index.idx " +
