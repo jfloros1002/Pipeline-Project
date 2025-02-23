ffc231726dd62c7c2d18c1171fcc200b2db2df99
John Floros <jfloros1002@gmail.com>
2025-02-22 21:29:45 -0600

    Added to Script

--- wrapper_script.py
+++ wrapper_script.py
@@ -2,5 +2,7 @@
 import os
 #Create a directory for output of all files
 os.system("mkdir PipelineProject_John_Floros")
+os.chdir("PipelineProject_John_Floros")
+HCMV_cds = os.system("datasets download virus  genome accession NC_006273.2 --include cds")
 
-reference_transcriptome = "kallisto index -1 index.idx " +
+#reference_transcriptome = "kallisto index -1 index.idx " +
